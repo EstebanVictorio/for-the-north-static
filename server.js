cf7f86ece8ee959775c120312c5dc0b76fc3769f
Esteban Victorio <estebanvictorio92@gmail.com>
2019-04-21 21:27:50 -0500

    [update] add posts pages, updated routing for pages

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const server = express()
+const env = require('dotenv')
 const pages = require('./routes/pages')
-const port = 3000
 
+env.config()
+
+const blogPostsRouter = express.Router()
+blogPostsRouter.route('/').get(pages.blogPosts)
+blogPostsRouter.route('/:postname').get(pages.blogPosts)
 
 
 server.use('/public',express.static('public'))
 server.use('/static',express.static('static'))
+
 server.get('/', pages.home)
+server.get('/about', pages.about)
+server.use('/posts',blogPostsRouter)
 
-
-server.listen(3000, function () {
-  console.log('> Started server on port 3000...')
+const port = process.env.PORT || 3000
+server.listen(port, function () {
+  console.log(`> Started server on port ${port}...`)
 })
