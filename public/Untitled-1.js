8dbf5f0a46d7028497f63274e1b80946dce69efc
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-08 08:30:30 -0500

    [update] updated ignore binding sample

--- public/Untitled-1.js
+++
@@ -1,9 +0,0 @@
-function checkIfEqual(a, b) {
-  let keys = Object.keys(a)
-  for(let i = 0; i < keys.length; i++){
-    if(!a[keys[i]]===b[keys[i]]){
-      return false
-    }
-  }
-  return true
-}
